95a4aac09fcab0287306500d2a337d833307e0d8
満園大輔 <a1@MacBook-Pro-3.local>
2019-04-24 08:58:39 +0900

    Implement basic login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,17 +1,26 @@
 module SessionsHelper
-    
-    def log_in(user)
-        session[:user_id] = user_id
-    end
 
-    def current_user
-      if session[:user_id]
-        @current_user ||= User.find_by(id:session[:user_id])
-      end
-    end
+# 渡されたユーザーでログインする
+def log_in(user)
+  session[:user_id] = user.id
+end
 
-    def logged_in?
-      !current_user.nil?
-    end
-    
+# 現在ログイン中のユーザーを返す (いる場合)
+def current_user
+  if session[:user_id]
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+end
+
+# ユーザーがログインしていればtrue、その他ならfalseを返す
+def logged_in?
+  !current_user.nil?
+end
+
+def log_out
+  session.delete(:user_id)
+  @current_user = nil
+end
+
+
 end
