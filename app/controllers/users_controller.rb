70799d5bb36f217160d1b99597173031ace916df
mitsuzono <daigoren5@gmail.com>
2019-05-08 12:47:36 +0900

    finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
-  
+  before_action :logged_in_user, only:[:edit,:update]
+  before_action :correct_user, only:[:edit,:update]
+
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
   end
-  
   
   def new
     @user = User.new
@@ -27,7 +32,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render'edit'
     end
@@ -44,5 +50,22 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password,:password_confirmation)
+  end
+
+  #beforeアクション
+  #ログイン済みのユーザかどうか確認 
+  
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  #正しいユーザかどうか確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url)unless current_user?(@user)
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,12 @@ def remember(user)
   cookies.permanent[:remember_token] = user.remember_token
 end
 
+#渡されたユーザがログイン済みのユーザであればtrueを返す
+def current_user?(user)
+  user == current_user
+end
+
+# 記憶トークンcookieに対応するユーザーを返す
 def current_user
   if (user_id = session[:user_id])
     @current_user ||= User.find_by(id: user_id)
@@ -47,5 +53,15 @@ def log_out
   @current_user = nil
 end
 
+# 記憶したURL (もしくはデフォルト値) にリダイレクト
+def redirect_back_or(default)
+  redirect_to(session[:forwarding_url] || default)
+  session.delete(:forwarding_url)
+end
+
+# アクセスしようとしたURLを覚えておく
+def store_location
+  session[:forwarding_url] = request.original_url if request.get?
+end
 
 end
